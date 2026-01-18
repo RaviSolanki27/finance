607b149b5504a6c174cca732b935b95d37de9837
RaviSolanki27 <ravisolanki9322@gmail.com>
2026-01-18 23:00:23 +0530

    started working  on accounts page

---
+++ src/app/(protected)/accounts/page.tsx
@@ -0,0 +1,16 @@
+import CommonDropdownCard from "@/components/common/CommonDropdownCard";
+import CommonUrlCard from "@/components/common/CommonUrlCard";
+
+const Accounts = () => {
+  return (
+    <div>
+      <div className="grid">
+        <CommonDropdownCard title="Total balance">Hello</CommonDropdownCard>
+      </div>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Accounts;
---
+++ src/components/common/CommonDropdownCard.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Card, CardHeader } from "../ui/card";
+import Link from "next/link";
+import { ArrowUpRight } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+
+const CommonDropdownCard = ({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title: string;
+}) => {
+  return (
+    <Card className="min-w-50 py-4 px-6 shadow-none relative">
+      <CardHeader className="p-0">
+        <div className="flex justify-between">
+          <span className="font-medium ">{title}</span>
+          <Select defaultValue={""}>
+            <SelectTrigger id="form-rhf-demo-type">
+              <SelectValue placeholder="Select a type" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="income">Income</SelectItem>
+              <SelectItem value="expense">Expense</SelectItem>
+              <SelectItem value="transfer">Transfer</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </CardHeader>
+
+      <>{children}</>
+    </Card>
+  );
+};
+
+export default CommonDropdownCard;
